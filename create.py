3dbf1938dd6b0869e913c694c99b7b815e534f16
Minh Nguyen <hongminh1620@gmail.com>
2020-04-10 16:38:51 +0700

    Create login and register html

--- create.py
+++ create.py
@@ -4,13 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker 
 import pandas as pd
 import datetime
+from web import db
 
-engine = create_engine('sqlite:///database.db')
-engine.connect() 
-
-Base = declarative_base() 
-
-class User(Base):
+class User(db.Model):
     __tablename__ = 'users'
     id = Column(Integer, primary_key = True)
     username = Column(Text, index = True)
@@ -21,7 +17,7 @@ class User(Base):
     def __repr__(self):
         return "<User(id={0}, username={1}, name ={2}, password={3}, email={4})".format(self.id, self.username, self.name, self.password, self.email)
 
-class Book(Base):
+class Book(db.Model):
     __tablename__ = 'books'
     id = Column(Integer, primary_key = True)
     title = Column(Text, index = True)
@@ -29,19 +25,25 @@ class Book(Base):
     language = Column(Text, index = True)
     owner = Column(id, ForeignKey('users.id'))
     current_owner = Column(id, ForeignKey('users.id'))
+    users = relationship('User')
 
     def __repr__(self):
         return "<Book(id={0}, title={1}, author_name={2}, language={3}, owner={4}, current_owner={5})".format(self.id, self.title, self.author_name, self.language, self.owner, self.current_owner)
 
-class Letter(Base):
+class Letter(db.Model):
     __tablename__ = 'letters'
     id = Column(Integer, primary_key = True)
     book_id = Column(id, ForeignKey('books.id'))
     user_id = Column(id, ForeignKey('users.id'))
     date = Column(Date, index=True)
     message = Column(Text, index = True)
-    
+    books = relationship('Book')
+    users = relationship('User')
+
     def __repr__(self):
         return "<Letter(id={0}, book_id={1}, user_id={2}, date={3}, message={4})".format(self.id, self.book_id, self.user_id, self.date, self.message)
 
+#create all tables and commit
+db.create_all()
+db.session.commit()
 
