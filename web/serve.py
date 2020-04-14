63a3430bbe1878d088133182e0475b8d81676331
wei-ting-yap <yap_weiting@hotmail.com>
2020-04-14 10:00:26 +0800

    Instantiates Flask, SQLAlchemy, routes and renders index.html

--- web/serve.py
+++ web/serve.py
@@ -3,30 +3,36 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
 
+# Initiate Flask app
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+# Create local SQL Lite database.db file in directory
+# db_path = os.path.join(os.path.dirname(__file__), 'database.db')
+# URI = 'sqlite:///{}'.format(db_path)
+URI = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
+# To use environment variable as URI - for final submission
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+# Instantiate SQLAlchemy
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
+    def __repr__(self):
+        return '<User {}: {}>'.format(self.id,self.name)
 
-db.create_all()
+db.create_all() #create all tables
+
 example_user = User(id=1, name="Philip Sterne")
-db.session.merge(example_user)
+db.session.add(example_user)
 db.session.commit()
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/users')
-def users():
-    users = User.query.all()
-    return render_template('users.html', users=users)
 
 if __name__ == '__main__':
     app.run()
