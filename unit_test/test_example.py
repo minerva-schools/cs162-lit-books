1f48fa404693174a47ce482ee802cceef748916b
Mohamedgad99 <37738652+Mohamedgad99@users.noreply.github.com>
2020-04-22 17:46:30 -0700

    Update test_example.py
    Unit-tests for the basic functions

--- unit_test/test_example.py
+++ unit_test/test_example.py
@@ -1,40 +1,70 @@
-# Example taken from:
-# http://flask.pocoo.org/docs/1.0/testing/
-# and suitably modified.
 import os
 import tempfile
-
+import unittest
 import pytest
 
 from web import app, db
+import app
 
 
-@pytest.fixture
-def client():
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    app.config['TESTING'] = True
-    client = app.test_client()
+class BasicTests(unittest.TestCase):
 
-    yield client
+    # executed prior to each test
+    #creates a new test client and initializes a new databade
+    def setUp(self):
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + TEST_DB
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
 
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
 
 
-def test_empty_db(client):
-    """Start with a blank database."""
 
-    rv = client.get('/')
-    assert b'Hello World!' in rv.data
+    # executed after each test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
-def login(client, username, password):
-    return client.post('/login', data=dict(
-    username="sho",
-    password="password"
-    ), follow_redirects=True)
+    # user authentication methods
 
-def logout(client):
-    return client.get('/logout', follow_redirects=True)
-        
+    def signup(self, username, password, email, confirm):
+        return self.app.post(
+            '/signup',
+            data=dict(username=username, password=password, email=email, confirm=confirm),
+            follow_redirects=True
+        )
+
+    def login(self, username, password):
+        return self.app.post(
+            '/login',
+            data=dict(username=username, password=password),
+            follow_redirects=True
+        )
+
+    def logout(self):
+        return self.app.get(
+            '/logout',
+            follow_redirects=True
+        )
+
+    #basic tests
+    # Standard Route tests
+    def test_valid_user_signup(self):
+        response = self.signup('test', 'test1', 'test@test.com')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_user_login(self):
+        response = self.login('test@test.com', 'test1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_user_logout(self):
+        response = self.logout()
+        self.assertEqual(response.status_code, 200)
+
+
+
 if __name__ == '__main__':
     unittest.main()
