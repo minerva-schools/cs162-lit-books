3de0325a5165f1c72beb9b4599c3fbaf36d6ca29
wei-ting-yap <yap_weiting@hotmail.com>
2020-04-25 07:16:58 +0800

    Added test_simple to test empty tables

--- unit_test/test_example.py
+++ unit_test/test_example.py
@@ -5,7 +5,7 @@ import pytest
 from flask_sqlalchemy import SQLAlchemy
 
 from web import app, db
-from web.create_db import User, Book, Letter, Current_Owner
+from web.create_db import User, Book, Letter, Current_Owner, BookTransactions
 
 app.config['TESTING'] = True #DB mounted on memory for specific app instance
 
@@ -19,7 +19,11 @@ def client():
 
 def test_simple(client):
     """Test empty db"""
+    assert db.session.query(User).count() == 0
     assert db.session.query(Book).count() == 0
+    assert db.session.query(Letter).count() == 0
+    assert db.session.query(Current_Owner).count() == 0
+    assert db.session.query(BookTransactions).count() == 0
 
 if __name__ == '__main__':
     unittest.main()
