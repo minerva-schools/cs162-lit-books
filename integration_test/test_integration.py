3d953628a5080e7bc03088f3a07ee8d50f429dfb
Kate Tanha <tanhakate@gmail.com>
2020-04-24 18:02:14 -0300

    Revert "created login-required decorater, added integration tests"
    This reverts commit ed8525ab284bbbc9b7928d6c0069aae301d28116.

--- integration_test/test_integration.py
+++ integration_test/test_integration.py
@@ -1,30 +1,6 @@
 import requests
-from web import app
+
 
 def test_basic_request():
     r = requests.get('http://127.0.0.1:5000/')
     assert r.status_code == 200
-
-
-def test_login_required_when_logged_out():
-	with app.test_client() as client:
-		response = client.get('/add')
-		assert response.status_code == 302
-
-		response = client.get('/user/lionel_messi')
-		assert response.status_code == 302
-
-def test_login_required_when_logged_in():
-	with app.test_client() as client:
-		username = 'tanhakate'	
-		password = '123456'
-		data = dict(username=username, password=password)
-		response = client.post('/login', data=data)
-		assert response.status_code == 302
-		assert 'tanhakate' in response.headers['Location']
-
-		#response = client.get('/add')
-		#assert response.status_code == 200
-
-		response = client.get('/user/tanhakate') #valid username
-		assert response.status_code == 200
--- web/serve.py
+++ web/serve.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
 import random
-import json, itsdangerous
-from functools import wraps
 
 from web import db,app
 from .create_db import User, Book, Letter
@@ -30,26 +28,6 @@ import hashlib
 # db.create_all() #create all tables
 #####################################################
 
-def login_required(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        username = session.get('username')
-        try:
-            _session = request.cookies["session"].partition('.')[0]
-        except KeyError:
-            session_username = None
-        else:
-            session_username = json.loads(itsdangerous.base64_decode(_session).decode('utf-8')).get('username')
-
-        print(username, session_username)
-        if username is None or session_username is None or username != session_username: #is checks for identity, since None is a singleton
-            flash ('Please log in to use')
-            return redirect(url_for('login'))
-
-        return func(*args, **kwargs)
-    return wrapper
-
-
 # Page to look up book by ID
 @app.route('/booksearch')
 def booksearch():
@@ -70,7 +48,6 @@ def about():
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.pop('username', None)
-    session.clear()
     flash('You were logged out')
     return redirect(url_for('login'))
 
@@ -159,11 +136,14 @@ def valid_book_id():
             return book_id
 ###############
 # Add a new book
-
 @app.route('/add', methods=["POST","GET"])
-@login_required
 def add_book():
     username = session.get('username')
+
+    if username == None:
+        flash ('Please log in to use')
+        return redirect(url_for('login'))
+
     user = db.session.query(User).filter(User.username == username).first()
     title = request.form['title']
     author = request.form['author']
@@ -178,13 +158,11 @@ def add_book():
 
 # Book listing
 @app.route('/book/id/<bookid>')
-@login_required
 def book(bookid):
     return render_template('book_page.html')
 
 # User profile by username
 @app.route('/user/<username>')
-@login_required
 def user_byusername(username):
     user = db.session.query(User).filter(User.username == username).first()
     name = user.name
