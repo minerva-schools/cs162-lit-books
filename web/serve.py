60a42fea85b6ce614ef3442860c8e958db338798
wei-ting-yap <yap_weiting@hotmail.com>
2020-04-25 02:18:36 +0800

    Added Current_Owners table and add to Current_Owners when book is added

--- web/serve.py
+++ web/serve.py
@@ -156,6 +156,7 @@ def add_book():
 
     new_book = Book(title=title, author_name=author, id=book_id, owner=user.id)
     db.session.add(new_book)
+    db.session.add(Current_Owner(id=book_id,owner=user.id)
     db.session.commit()
     return redirect(url_for('book', bookid = book_id))
 
@@ -171,6 +172,10 @@ def user_byusername(username):
     if username == None:
         username = session.get('username')
     user = db.session.query(User).filter(User.username == username).first()
+    user_id = user.id
+    shared = db.session.query(BookTransactions
+                        ).filter_by(BookTransactions.to_user_id == user_id
+                        ).
     return render_template('users.html', user=user)
 
 # User profile by id
